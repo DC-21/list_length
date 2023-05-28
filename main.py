bf763d3a685fc3c6341b078d0f5873a70546159d
DC-21 <cholahkuboko@gmail.com>
2023-05-28 13:51:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def find_list_length(lst):
+    length = len(lst)
+    return length
+
+# Example usage:
+my_list = [1, 2, 3, 4, 5]
+list_length = find_list_length(my_list)
+print("Length of the list:", list_length)
